90e7a8f147feaa11e0627a2902310d28037ddb47
hazemuh <haikalzhafran05@gmail.com>
2021-12-25 22:25:12 +0700

    update nama dan button sedikit

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -1,6 +1,6 @@
 <head>
 	<meta charset="utf-8">
-	<title>Codeigniter Gallery with jQuery File Upload</title>
+	<title>MyGallery</title>
 
 	<link rel="stylesheet" href="<?php echo base_url()?>application/third_party/libs/css/bootstrap.min.css">
 	<script src="<?php echo base_url()?>application/third_party/libs/js/jquery-1.9.1.min.js"></script>
--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="<?php echo base_url()?>">CI Gallery</a>
+      <a class="navbar-brand" href="<?php echo base_url()?>">MyGallery</a>
     </div>
     <ul class="nav navbar-nav">
       <li <?=$home_active?> ><a href="<?php echo base_url()?>">Home</a></li>
@@ -10,7 +10,7 @@
     </ul>
     <ul class="nav navbar-nav navbar-right" style="padding-right: 20px;">    
       <a class="btn btn-info navbar-btn" href="<?php echo base_url().'album/create'?>">Add New Album</a>
-      <a class="btn btn-info" style="background-color: maroon; color: white;" href="<?=base_url('albums/logout')?>">Sign Out</a>
+      <a class="btn btn-info navbar-btn" style="background-color: maroon; color: white; border-color:maroon; margin-left:10px; " href="<?=base_url('albums/logout')?>">Sign Out</a>
     </ul>
   </div>
 </nav>
